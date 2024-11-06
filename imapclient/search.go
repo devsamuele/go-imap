abfd0fd9ff0234ffc8cc0e7ddad4993aeb339523
samuele <dev.samuele@gmail.com>
2024-11-06 11:19:27 +0100

    update

--- imapclient/search.go
+++ imapclient/search.go
@@ -126,12 +126,12 @@ func (c *Client) UIDSearch(criteria *imap.SearchCriteria, options *imap.SearchOp
 
 // Search sends a SEARCH command.
 func (c *Client) SearchXGMEXT1(arg string, options *imap.SearchOptions) *SearchCommand {
-	return c.searchXGMEXT1(imapwire.NumKindSeq, arg, options)
+	return c.searchXGMEXT1(imapwire.NumKindSeq, arg)
 }
 
 // UIDSearch sends a UID SEARCH command.
 func (c *Client) UIDSearchXGMEXT1(args string, options *imap.SearchOptions) *SearchCommand {
-	return c.searchXGMEXT1(imapwire.NumKindUID, args, options)
+	return c.searchXGMEXT1(imapwire.NumKindUID, args)
 }
 
 func (c *Client) handleSearch() error {
